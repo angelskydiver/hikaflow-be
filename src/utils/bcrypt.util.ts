3bb72ce082164489a28f1e8b62dffbe686166b49
angelskydiver <158997505+angelskydiver@users.noreply.github.com>
2025-01-12 17:04:20 +0500

    Update bcrypt.util.ts

--- src/utils/bcrypt.util.ts
+++ src/utils/bcrypt.util.ts
@@ -1,5 +1,8 @@
 import * as bcrypt from 'bcryptjs';
 
+
+let hash="adflnadkfrrkfnks"
+
 // Hash a plain password
 export const hashPassword = async (password: string): Promise<string> => {
   const salt = await bcrypt.genSalt(10); // Generate a salt
