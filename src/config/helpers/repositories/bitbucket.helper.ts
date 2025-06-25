3a0fb719d83a66fadd2d032318d0652c39c63c7a
angelskydiver <158997505+angelskydiver@users.noreply.github.com>
2025-06-26 02:38:33 +0500

    fix: urgent bitbucket fix

--- src/config/helpers/repositories/bitbucket.helper.ts
+++ src/config/helpers/repositories/bitbucket.helper.ts
@@ -1,6 +1,6 @@
 import * as atlassianJwt from 'atlassian-jwt';
 import axios from 'axios';
-import moment from 'moment';
+import * as moment from 'moment';
 import {
   ignoredExtensionsForFileScan,
   ignoredFilesForFileScan,
